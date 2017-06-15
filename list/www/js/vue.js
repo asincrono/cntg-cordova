bfa26c681f5f38f0f74fa5e31be58325cba670e0
Manuel C. Piñeiro Mourazos <asincrono@gmail.com>
2017-06-15 20:32:42 +0200

    Adding thingies

--- list/www/js/vue.js
+++ list/www/js/vue.js
@@ -2,21 +2,75 @@ new Vue({
   el: "#things",
   data: {
     thing: {
-      name: 'no name',
-      description: 'no description',
-      date: 'no date'
+      name: '',
+      description: '',
+      date: ''
     },
+    updating: false,
+    updatingIndex: null,
     things: []
   },
+
+  beforeCreate: function () {
+    console.log('Before create')
+  },
+
+  created: function () {
+    this.things = JSON.parse(localStorage.getItem('savedThings')) || []
+    console.log('Before create')
+  },
+
+  beforeMount: function () {
+/*    const things = JSON.parse(localStorage.getItem('savedThings')) || []
+    this.$set(this, 'things', things)*/
+    console.log('Before mount')
+  },
+
+  mounted: function () {
+    console.log('Mounted')
+  },
+
   methods: {
     addThing: function () {
       this.thing.date = new Date()
+      /*const thing = {
+        name: this.thing.name,
+        description: this.thing.description,
+        date: this.thing.date
+      }*/
       this.things.push(this.thing)
+      this.saveToStorage()
+      this.thing = { name: '', description: '', date: '' }
+    },
+
+    saveToStorage: function () {
+      localStorage.setItem('savedThings', JSON.stringify(this.things))
+    },
+
+    updateValue: function () {
+      this.things.splice(this.updatingIndex, 1, this.thing)
+      this.saveToStorage()
     },
 
     submitForm: function () {
-      this.thing.date = new Date()
-      this.things.push(this.thing)
+      this.addThing()
+    },
+
+    deleteThing: function (index) {
+      // this.things.splice(index, 1)
+      Vue.delete(this.things, index)
+      this.saveToStorage()
+    },
+
+    updateThing: function (index) {
+      /*const thing = {
+        name: this.thing.name,
+        description: this.thing.description,
+        date: this.thing.date
+      }*/
+      this.$set(this, 'updating', true)
+      this.thing = this.things[index]
+      this.updatingIndex = index
     }
   }
 })
